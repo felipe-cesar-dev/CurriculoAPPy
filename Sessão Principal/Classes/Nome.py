29aee50acd6baa7653ecb39705355013443d79ae
Felipe César Porto Oliveira <136658021+felipe-cesar-dev@users.noreply.github.com>
2024-12-13 20:16:08 -0300

    Classe Nome 2

--- Sessão Principal/Classes/Nome.py
+++ Sessão Principal/Classes/Nome.py
@@ -1,3 +1,5 @@
+import re
+
 class Nome:
     def __init__(self, nome = ''):
         self.nome = nome
@@ -7,13 +9,19 @@ class Nome:
             raise ValueError("Nome não pode estar vazio")
         return nome.title()
 
+    def contem_numero(self, string):
+        checagem =  bool(re.search(r'\d', string))
+        if checagem:
+            raise ValueError
+
     def set_nome(self):
         while True:
             try:
                 self.nome = self.valida_nome(input("Digite seu nome: "))
+                self.contem_numero(self.nome)
                 break
-            except ValueError as e:
-                print(e)
+            except ValueError:
+                print('Nome não pode ser em branco ou conter números')
 
     def get_nome(self):
         return self.nome
---
+++ Sessão Principal/Classes/teste.py
@@ -0,0 +1,5 @@
+import re
+
+nome = 'fefefe'
+
+print(bool(re.search(r'\d',nome)))
